acb0de8f11a1ec984e72377f38afe062e3c60dec
Keshab Acharya <acharyakeshab1997@gmail.com>
2026-04-15 22:51:24 -0400

    fix application yml

--- services/event-service/src/main/java/com/eventhub/events/entity/Event.java
+++ services/event-service/src/main/java/com/eventhub/events/entity/Event.java
@@ -1,12 +1,21 @@
 package com.eventhub.events.entity;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-@Data
+@Table(name = "event")
+@Getter @Setter @Builder
+@NoArgsConstructor @AllArgsConstructor
 public class Event {
 
     @Id
